3ac3654f6e846ecc0e3c903adbac2277715fa845
gerarovetta <139300107+gerarovetta@users.noreply.github.com>
2023-09-28 09:26:56 -0300

    Some explanations updates

--- zoo/zoo.js
+++ zoo/zoo.js
@@ -6,12 +6,14 @@ import Coati from './animals/Coati.js';
 
 class Zoo {
   constructor() {
-    // Create instances of various animal types
+    // Create instances of various animal types 
+    // since this is an excercise we are just instantiating some animals in the Zoo constructor , 
+    // but for real project we might consider a collection of animals and methods to manage the animal collection
     this.lion = new Lion();
     this.tiger = new Tiger();
     this.elephant = new Elephant();
     this.coati = new Coati();
-    // Add other animal instances as needed * See notes on Readme
+    // Add other animal instances as needed 
   }
   // Here we can define other methods or functionality to manage the zoo
 }
